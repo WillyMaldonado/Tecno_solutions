9d29c948231fe9473d916e98fcdcb34679f4f9a0
Guillermo Maldonado <124693730+WillyMaldonado@users.noreply.github.com>
2023-03-14 15:45:06 -0600

    update main.py
    Pedi dos numeros al usuario y asigne el mayor a la variable mayor

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 mayor = 0
 residuo = 0
 # Guillermo Maldonado
-
-
+num_user1 = int(input('Ingrese un numero entero '))
+num_user2 = int(input('Ingrese el segundo numero entero '))
+if num_user1 > num_user2:
+    mayor = num_user1
+else:
+    mayor = num_user2
+print(mayor)
 # Grecia Fuentes
 
 
