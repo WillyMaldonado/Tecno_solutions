9062744571b235fa0fa125b802aca2789220f4fc
Grecia Fuentes <greciafuentes307@gmail.com>
2023-03-14 09:53:18 -0600

    update main
    agregue mi porcion de codigo
    hice la comprobacion del residuo

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ residuo = 0
 
 
 # Grecia Fuentes
+residuo = mayor % 2
 
+if residuo == 0:
+    print(f'El numero {mayor} es par porque su residuo es 0')
+else:
+    print(f'El numero {mayor} es impar porque su residuo es {residuo}')
 
 # Diego Gomez
 
